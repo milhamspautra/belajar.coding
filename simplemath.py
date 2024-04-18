65209feca03ec71ba23fdedacb1099b27777cad0
milhamspautra <ilhamsptr0529@gmail.com>
2024-04-18 21:12:28 +0700

    menambah file

---
+++ simplemath.py
@@ -0,0 +1,20 @@
+a = int(input("masukkan angka #1"))
+b = int(input("masukkan angka #2"))
+penjumlahan = a+b
+print(penjumlahan)
+
+a = int(input("masukkan angka #1"))
+b = int(input("masukkan angka #2"))
+pengurangan = a-b
+print(pengurangan)
+
+a = int(input("masukkan angka #1"))
+b = int(input("masukkan angka #2"))
+perkalian = a*b
+print(perkalian)
+
+a = int(input("masukkan angka #1"))
+b = int(input("masukkan angka #2"))
+pembagian = a/b
+print(pembagian)
+

0b93d42f4510c190b4a5369779e336ee54382cf6
milhamspautra <ilhamsptr0529@gmail.com>
2024-05-01 23:27:38 +0700

    mempelajari materi string

--- string.py
+++ string.py
@@ -1,10 +1,10 @@
 #count,find,format,isdigit,join,replace,rsplit
-nama="muhammad ilham saputra"
+nama=" muhammad ilham saputra "
 print("count",nama.count("u"))
 print("find",nama.find("mu"))
-print("format",nama.format())
+print("nama lengkap saya adalah {}".format(nama))
 print("isdigit",nama.isdigit())
-print("join",nama.join())
-print("replace",nama.replace())
-print("rsplit",nama.rsplit())
+print("-".join(nama))
+print(nama.replace("muhammad","tuan"))
+print(nama.rsplit("a"))
 
